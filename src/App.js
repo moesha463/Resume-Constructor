3c29c55c1bbac11354d439234c497a3e121f5d99
moesha463 <smirnovzena463@gmail.com>
2025-04-09 00:13:35 +0300

    feat: add libraries for resume constructor and create example for creating pdf

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import MyDocument from "./components/MyDocument";
+import React from 'react';
+import { PDFViewer, PDFDownloadLink } from '@react-pdf/renderer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <PDFViewer width="50%" height="600px">
+        <MyDocument />
+      </PDFViewer>
+
+      <PDFDownloadLink document={<MyDocument />} fileName="example.pdf">
+        {({ loading }) => (loading ? 'Генерация PDF...' : 'Скачать PDF')}
+      </PDFDownloadLink>
+    </>
   );
 }
 
